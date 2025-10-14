e80cef5932a63393f3049bf4f656fa46fccbde6b
DEmcla <22529028+DEmcla@users.noreply.github.com>
2025-10-14 07:01:12 -0400

    SECURITY: Remove all instructor content and solutions from public repository
    - Remove entire instructor/ directory with lesson plans and solutions
    - Remove all *_solution.cpp files from homework-hints
    - Update .gitignore to prevent instructor content from being re-added
    - Maintains academic integrity by keeping solutions private
    
    Total removed: 130+ instructor files including all assignment solutions

--- instructor/Book_assignments/listing_2_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-int main() {
-    unsigned short a = 0b10101010;
-    printf("%hu\n", a);
-    int b = 0123;
-    printf("%d\n", b);
-    unsigned long long d = 0xFFFFFFFFFFFFFFF;
-    printf("%llu\n", d);
-}
